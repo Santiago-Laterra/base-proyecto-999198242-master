d0102fbcd422d7bc1b91044b37f35838616ebd7f
Santiago Laterra <santigolaterra@gmail.com>
2025-08-07 16:02:14 -0300

    add:comentarios para identificar el cuerpo del codigo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,18 @@ const Footer = () => {
   return (
     <footer className="bg-amber-900 text-orange-100 py-6 mt-10">
       <div className="flex flex-col flex-row justify-between items-center px-6">
+        {/* Izquierda */}
         <div className="text-center text-sm"> Desarrollado por&nbsp;
           <a href="https://www.linkedin.com/in/santiago-laterra-003511323/" className="hover:text-orange-200 transition"
             target="_blank">@Santiago Laterra</a>
         </div>
-
+        {/* centro */}
         <div className="space-x-4">
           <a href="#">Github</a>
           <a href="#">Terminos y Condiciones</a>
           <a href="#">Privacidad</a>
         </div>
+        {/* Derecha */}
         <div>
           © 2025 Proyecto UTN. Todos los derechos reservados
         </div>
