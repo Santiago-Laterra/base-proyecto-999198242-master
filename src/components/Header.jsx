583d61f69d8bcfc381dfaffbbd320b2ab862580a
Santiago Laterra <santigolaterra@gmail.com>
2025-08-07 12:41:38 -0300

    add:agregue bg, sombra al header y cambie logica de los botones para no repetir codigo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,29 +9,36 @@ const Header = () => {
   }
 
   return (
-    <header style={{ backgroundColor: "lightblue" }}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="imagen de logo" />
-      <nav>
-        <ul>
-          {/* Cambiar elementos a por componentes Link de react-router-dom */}
-          {
-            user && <>
-              <li><Link to="/">Inicio</Link></li>
-              <li><Link to="/dashboard">Dashboard</Link></li>
-              <button onClick={handleLogout}>Cerrar sesión</button>
-            </>
-          }
-          {
-            !user && <>
-              <li><Link to="/">Inicio</Link></li>
-              <li><Link to="/nosotros">Nosotros</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/registrate">Registrate</Link></li>
 
-            </>
-          }
-        </ul>
-      </nav>
+
+    <header className="bg-[#e19f41] shadow-md">
+      <div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png"
+            alt="imagen de logo"
+          />
+        </div>
+        <nav>
+          <ul>
+            {/* Cambiar elementos a por componentes Link de react-router-dom */}
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/nosotros">Nosotros</Link></li>
+            {
+              user && <>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <button onClick={handleLogout}>Cerrar sesión</button>
+              </>
+            }
+            {
+              !user && <>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/registrate">Registrate</Link></li>
+              </>
+            }
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
