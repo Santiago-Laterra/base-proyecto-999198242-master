1bd5562fbd685728101bea122a0453a451bd9759
Santiago Laterra <santigolaterra@gmail.com>
2025-08-07 16:58:44 -0300

    change:de flex a grid para manejar el centrado correctamente y add:responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 const Footer = () => {
   return (
     <footer className="bg-amber-900 text-orange-100 py-6 mt-10">
-      <div className="flex flex-col flex-row justify-between items-center px-4">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 items-center text-center gap-4 md:gap-0">
 
         {/* Izquierda */}
-        <div className="text-center text-sm"> Desarrollado por&nbsp;
+        <div className="text-center text-sm md:text-left"> Desarrollado por&nbsp;
           <a href="https://www.linkedin.com/in/santiago-laterra-003511323/" className="hover:text-orange-200 transition"
             target="_blank">@Santiago Laterra</a>
         </div>
 
         {/* centro */}
-        <div className="flex space-x-4 text-base">
+        <div className="space-x-4 text-sm">
           <a href="https://github.com/Santiago-Laterra"
             className="hover:underline hover:text-orange-400 transition">Github</a>
           <a href="#"
@@ -20,7 +20,7 @@ const Footer = () => {
         </div>
 
         {/* Derecha */}
-        <div>
+        <div className="text-sm md:text-right">
           ©2025 Proyecto UTN. Todos los derechos reservados
         </div>
       </div>
